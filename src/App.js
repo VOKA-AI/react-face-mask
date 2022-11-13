79cca44bb19990b8ca1ee52872d9191066855d9e
tianxu <aoxiaotian@foxmail.com>
2022-11-13 22:34:11 +0800

    three-canvas和video重合，下一次可以用face的位置改变动画的位置

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
 import Model2 from './Model2'
 import { Canvas } from '@react-three/fiber'
+import Model from './Model'
 
 function App() {
   return (
-    <div id="main">
-      <div class="container">
-        <div class="canvas-wrapper">
-          <canvas id="output"></canvas>
-          <video id="video"></video>
+      <div class="canvas-wrapper">
+        <div class="three-wrapper" style={{
+           width: "640px",
+           height: "480px",
+           position: 'fixed',
+           zIndex:2,
+         }}>
+          <Canvas>
+            <ambientLight />
+            <Model />
+          </Canvas>
         </div>
+
+        <canvas id="output" style = {{
+          position: 'fixed',
+          zIndex: 3,
+        }} width = "640px" height = "480px"></canvas>
+
+        <video id="video" style={{
+          position: 'fixed',
+          zIndex: 1,
+        }} width = "640px" height = "480px" ></video>
       </div>
-    </div>
   );
 }
 
