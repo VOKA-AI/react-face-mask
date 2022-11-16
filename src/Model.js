dd19112934903b4db780bb919d73aeb1db27752c
tianxu <aoxiaotian@foxmail.com>
2022-11-16 22:30:47 +0800

    可以实现简单的面部跟踪了，但还是需要解决如下问题： 1. 抖动问题 2. 面部转动时，脸部位置的判定也会发生改变

--- src/Model.js
+++ src/Model.js
@@ -1,15 +1,30 @@
 import React, { useEffect } from "react"
 import { useGLTF, useTexture, useAnimations } from "@react-three/drei"
 
+
+
+let _actions;
+let _names;
+
+export function start_play() {
+  _actions[_names[2]].reset().play()
+}
+
+export function stop_play() {
+  _actions[_names[2]].reset().stop()
+}
+
 export default function Model(props) {
   // Fetch model and a separate texture
   const { nodes, animations } = useGLTF("/stacy.glb")
   const texture = useTexture("/stacy.jpg")
   // Extract animation actions
   const { ref, actions, names } = useAnimations(animations)
-  useEffect(() => {
-    actions[names[2]].reset().play()
-  })
+  _actions = actions;
+  _names = names;
+//   useEffect(() => {
+//     actions[names[2]].reset().play()
+//   })
   return (
     <group ref={ref} dispose={null} rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
       
