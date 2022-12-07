750b66f7f44182917f1be984c919aef8ec0cc858
tianxu <aoxiaotian@foxmail.com>
2022-12-07 15:57:27 +0800

    增加新的模型

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import { Canvas } from '@react-three/fiber'
 import FaceFollower from './FaceFollower';
 
-const drawWidth = "640px";
-const drawHeight = "480px";
+const drawWidth = "1280px";
+const drawHeight = "960px";
 const videoID = "video";
 const outputCanvasID = "output";
 
